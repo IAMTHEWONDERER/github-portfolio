672be2f1b35393383c1a4f2de4b521a1ded5d5b1
IAMTHEWONDERER <theoblivionitself@gmail.com>
2024-03-26 01:59:32 +0000

    " react portfolio"

---
+++ portfolio/src/data/seo.js
@@ -0,0 +1,38 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using Node.js, Express.js and MongoDB.",
+		keywords: ["Oussama", "Oussama A", "Oussama Alouche"],
+	},
+
+	{
+		page: "about",
+		description:
+		"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using Node.js, Express.js and MongoDB.",
+		keywords: ["Oussama", "Oussama A", "Oussama Alouche"],
+	},
+
+	{
+		page: "articles",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, UI/UX design and more.",
+			keywords: ["Oussama", "Oussama A", "Oussama Alouche"],
+		},
+
+	{
+		page: "projects",
+		description:
+			"I've worked on a variety of mini projects over the last 3 months and I'm proud of the progress I've made. Many of these mini projects are open-source and available for on Github.",
+			keywords: ["Oussama", "Oussama A", "Oussama Alouche"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+			keywords: ["Oussama", "Oussama A", "Oussama Alouche"],
+	},
+];
+
+export default SEO;
---
+++ portfolio/src/data/user.js
@@ -0,0 +1,84 @@
+const INFO = {
+	main: {
+		title: "Portfolio by Oussama Alouche",
+		name: "Oussama ALOUCHE",
+		email: "theoblivionitself@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/IAMTHEWONDERERXD",
+		linkedin: "https://linkedin.com/oussama-alouche",
+		instagram: "https://instagram.com/iamthewonderer",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web developer and UI/UX designer, and a physics enthusiast.",
+		description:
+			"I am a fullstack developer with knowledge in MERN stack. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Oussama ALOUCHE, I live in Morocco, where I make my own Future.",
+		description:
+			"I've worked on a variety of mini projects over the last 3 months since i started my journey.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/react/react.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/nodejs/nodejs.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/mongodb/mongodb.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
